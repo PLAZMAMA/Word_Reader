4b473d9a0a49c69b8318d842c3959638e24337da
Maor Cohen <maor@Shaloms-MacBook-Air.local>
2019-05-03 10:25:11 -0400

    fixed the vert_crop function

--- code_files/Image.py
+++ code_files/Image.py
@@ -10,14 +10,21 @@ class Image():
         self.image = PIL.ImageOps.invert(self.image) #inverts the color of the image to help is the cutting process
         self.image = preprocessing.normalize(self.image) #normalizes the image for the model
 
-    def vert_crop(self):
-        """crops the vertical whitespace of the image"""
+    def part_vert_crop(self, img):
+        """crops the starting vertical whitespace of the image"""
         crop_img = []
-        for row in self.image:
+        for row in img:
             if np.sum(row) != 0:
+                crop_img.append(row)
+            elif len(crop_img) != 0:
                 crop_img.append(row)
 
         return(crop_img)
+
+    def vert_crop(self):
+        croppeed_image = self.part_vert_crop(self.image) #calls the function to remove the whitespace ontop
+        croppeed_image = self.part_vert_crop(croppeed_image[::-1]) #calls the function to remove the whitespace on the bottom
+        cropped_image = cropped_image[::-1] #since the funtion returns the image flipped because we gave it the image flipped, we are flipping it back
 
     def to_letters(self):
         """seperates the image of the word to the same sequence of letters"""
