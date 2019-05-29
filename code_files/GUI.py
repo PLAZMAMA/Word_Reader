8b0d485971d528ce112568622525e0811bf3ad5c
Mahi <47994802+Mahisaurus-Rex@users.noreply.github.com>
2019-05-29 18:33:31 -0400

    Update GUI.py

--- code_files/GUI.py
+++ code_files/GUI.py
@@ -1,25 +1,38 @@
 #Mahi and Hargun
 from tkinter import * 
-
+from tkinter import scrolledtext
 
 class GUI:
     def __init__(self, master):
         self.master = master
         master.title("Word Reader")
+        
+        #Canvas with image and reader
+        self.image_view = Canvas(master, width=200, height=100)
+        self.image_view.pack()
+        self.image_view.create_rectangle(50, 25, 150, 75, fill="blue")
 
-        self.img_view = Canvas(master,width=200, height=100)
-        self.img_view.pack()
-        self.img_view.create_rectangle(50, 25, 150, 75, fill="blue")
+        #Convert button
+        self.convert_button = Button(master, text="Convert", command=self.convert)
+        self.convert_button.pack()
 
-        self.greet_button = Button(master, text="Greet", command=self.greet)
-        self.greet_button.pack()
+        #copy button
+        self.copy_button = Button(master, text="Copy", command=self.copy)
+        self.copy_button.pack()
 
-        self.close_button = Button(master, text="Close", command=master.quit)
-        self.close_button.pack()
+        #Output textbox
+        self.textbox = scrolledtext.ScrolledText(self.master,width=40,height=10)
+        self.textbox.pack()
 
-    def greet(self):
-        print("Greetings!")
 
+    #fuction for convert button
+    def convert(self):
+        print("convert")
+    #function for copy button
+    def copy(self):
+        print("copy")
+
+        
 root = Tk()
 my_gui = GUI(root)
 root.mainloop()
